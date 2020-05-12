e247850416ccd365f0963a9c0639cbe5326a372b
Wolfgang Moser <6838235+wolfm89@users.noreply.github.com>
2020-05-12 15:07:10 +0200

    Add LAN

--- loadtest/loadtest.tf
+++ loadtest/loadtest.tf
@@ -1,3 +1,9 @@
+resource "profitbricks_lan" "loadtest_lan" {
+  name          = "loadtest"
+  datacenter_id = var.datacenter
+  public        = true
+}
+
 resource "profitbricks_server" "loadtest_server" {
   count             = var.server_count
   name              = "loadtest-server-${count.index}"
@@ -6,14 +12,16 @@ resource "profitbricks_server" "loadtest_server" {
   ram               = 4096
   availability_zone = "ZONE_1"
   cpu_family        = "INTEL_XEON"
+  image_name        = var.image_name
+
   volume {
-    name         = "hdd-loadtest-${count.index}"
-    size         = var.hdd_size
-    disk_type    = "HDD"
-    image_name   = var.image_name
+    name      = "hdd-loadtest-${count.index}"
+    size      = var.hdd_size
+    disk_type = "HDD"
   }
+
   nic {
-    lan             = var.lan_id
+    lan             = profitbricks_lan.loadtest_lan.id
     dhcp            = true
     firewall_active = true
 
