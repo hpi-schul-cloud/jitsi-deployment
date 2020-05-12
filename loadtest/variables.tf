ca263e216c1f4ebb919bccee827dd023bf3c8b6f
Wolfgang Moser <6838235+wolfm89@users.noreply.github.com>
2020-05-12 15:07:10 +0200

    Add LAN

--- loadtest/variables.tf
+++ loadtest/variables.tf
@@ -1,13 +1,13 @@
-variable "server_count" {
-  description = "number of loadtest servers"
-  type        = number
-  default     = 2
-}
-
 variable "datacenter" {
   description = "data center ID where loadtest servers should be created"
   type        = string
   default     = "df8bd51c-298f-4981-8df8-6fd07340b397" # Video Data Center
+}
+
+variable "server_count" {
+  description = "number of loadtest servers"
+  type        = number
+  default     = 2
 }
 
 variable "hdd_size" {
@@ -20,10 +20,4 @@ variable "image_name" {
   description = "image name"
   type        = string
   default     = "517aa2f9-8cfb-4a11-9893-db0421be1c5e" # Jitsi Torturer
-}
-
-variable "lan_id" {
-  description = "LAN ID where to provision the loadtest servers"
-  type        = string
-  default     = "4" # LAN 4
 }
