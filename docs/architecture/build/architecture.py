eb615e3c31a5a56ad46921b9c25f328324614d0a
Wolfgang Moser <6838235+wolfm89@users.noreply.github.com>
2020-05-14 17:51:14 +0200

    Update diagram

--- docs/architecture/build/architecture.py
+++ docs/architecture/build/architecture.py
@@ -1,31 +1,41 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from diagrams import Diagram
+from diagrams import Diagram, Cluster
 from diagrams.custom import Custom
 from diagrams.k8s.clusterconfig import HPA
 from diagrams.k8s.compute import Deployment, Pod, ReplicaSet, StatefulSet
 from diagrams.k8s.group import Namespace
 from diagrams.k8s.network import Ingress, Service
 
-with Diagram("Jitsi Meet", show=False):
-    Namespace("jitsi")
+globe_img = "resources/globe.png"
 
-    browser = Custom("browser", "resources/globe.png")
-    ingress = Ingress("jitsi.messenger.schule")
+with Diagram(filename="jitsi_meet", direction='LR', show=False):
+    with Cluster("Conference 1"):
+        users_1 = [Custom("user", globe_img) for _ in range(3)]
+    with Cluster("Conference 2"):
+        users_2 = [Custom("user", globe_img) for _ in range(2)]
 
-    browser >> ingress
+    all_users = Custom("all users", globe_img)
 
-    jitsi_pods = [Pod("jitsi")]
-    ingress >> Service("web") >> jitsi_pods << ReplicaSet("jitsi") << Deployment("jitsi")
-    prosody_service = Service("prosody")
-    prosody_service >> jitsi_pods
+    with Cluster("Namespace 'jitsi'"):
+        Namespace("jitsi")
+        ingress = Ingress("jitsi.messenger.schule")
 
-    n_jvbs = 3
-    jvb_pods = [Pod(f"jvb-{i}") for i in range(n_jvbs)]
-    jvb_services = [Service(f"jvb-{i}") for i in range(n_jvbs)]
-    [jvb_services[i] >> jvb_pods[i] >> prosody_service for i in range(n_jvbs)]
-    jvb_pods << StatefulSet("jvb") << HPA("hpa")
+        all_users >> ingress
 
-    browser >> jvb_services
+        jitsi_pods = [Pod("jitsi")]
+        ingress >> Service("web") >> jitsi_pods << ReplicaSet("jitsi") << Deployment("jitsi")
+        prosody_service = Service("prosody")
+        prosody_service >> jitsi_pods
+
+        n_jvbs = 3
+        with Cluster("Jitsi Videobridge"):
+            jvb_pods = [Pod(f"jvb-{i}") for i in range(n_jvbs)]
+            jvb_services = [Service(f"jvb-{i}") for i in range(n_jvbs)]
+        [jvb_services[i] >> jvb_pods[i] >> prosody_service for i in range(n_jvbs)]
+        jvb_pods << StatefulSet("jvb") << HPA("hpa")
+
+        users_1 >> jvb_services[0]
+        users_2 >> jvb_services[1]
 
