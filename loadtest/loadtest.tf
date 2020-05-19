2f7d5ff30b1cc1bb717ae30f729d174c928a4eef
Max <maximilian.kertel@woodmark.de>
2020-05-19 14:26:42 +0200

    Change Jitsi-Dashboard, increase number of cores for test server

--- loadtest/loadtest.tf
+++ loadtest/loadtest.tf
@@ -8,7 +8,7 @@ resource "profitbricks_server" "loadtest_server" {
   count             = var.server_count
   name              = "loadtest-server-${count.index}"
   datacenter_id     = var.datacenter
-  cores             = 4
+  cores             = 16
   ram               = 16384
   availability_zone = "ZONE_1"
   cpu_family        = "INTEL_XEON"
