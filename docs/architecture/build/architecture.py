817c40c313d6930ddaf20da1b372ebb779a804e4
Max <maximilian.kertel@woodmark.de>
2020-05-26 12:21:22 +0200

    Update architecture image
    Update architecture image as jitsi-Pod is now split up. Made slight changes (changed directed edges to nondirected) and removed namespace symbol as that information is redundant

--- docs/architecture/build/architecture.py
+++ docs/architecture/build/architecture.py
@@ -5,7 +5,6 @@ from diagrams import Diagram, Cluster
 from diagrams.custom import Custom
 from diagrams.k8s.clusterconfig import HPA
 from diagrams.k8s.compute import Deployment, Pod, ReplicaSet, StatefulSet
-from diagrams.k8s.group import Namespace
 from diagrams.k8s.network import Ingress, Service
 
 globe_img = "resources/globe.png"
@@ -23,15 +22,21 @@ with Diagram(filename="jitsi_meet", direction='LR', show=False, outformat='png',
     all_users = Custom("all users", globe_img)
 
     with Cluster("Namespace 'jitsi'"):
-        Namespace("jitsi")
         ingress = Ingress("jitsi.messenger.schule")
 
         all_users >> ingress
 
-        jitsi_pods = [Pod("jitsi")]
-        ingress >> Service("web") >> jitsi_pods << ReplicaSet("jitsi") << Deployment("jitsi")
+        web_pod = Pod("web")
+        prosody_pod = Pod("prosody")
+        jicofo_pod = Pod("jicofo")
+        Deployment("prosody") >> prosody_pod
+        Deployment("jicofo") >> jicofo_pod
+        Deployment("web") >> web_pod
+        ingress >> Service("web") >> web_pod
         prosody_service = Service("prosody")
-        prosody_service >> jitsi_pods
+        prosody_service - web_pod
+        prosody_service - prosody_pod
+        prosody_service - jicofo_pod
 
         n_jvbs = 3
         with Cluster("Jitsi Videobridge"):
